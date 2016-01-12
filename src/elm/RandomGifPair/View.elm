150538c0ad00bbbe84d18b0a52816ef8887b7329
Charles Watson <sircharleswatson@gmail.com>
2016-01-12 14:34:45 -0600

    update html grid

--- src/elm/RandomGifPair/View.elm
+++ src/elm/RandomGifPair/View.elm
@@ -1,6 +1,7 @@
 module RandomGifPair.View where
 
 import Html exposing (Html, div)
+import Html.Attributes exposing (class)
 
 import RandomGif.View as RandomGif exposing (view)
 import RandomGifPair.Model exposing (Model)
@@ -9,7 +10,9 @@ import RandomGifPair.Update exposing (Action)
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
-    [ RandomGif.view (Signal.forwardTo address RandomGifPair.Update.Left) model.left
-    , RandomGif.view (Signal.forwardTo address RandomGifPair.Update.Right) model.right
+  div [ class "ui sixteen column grid" ]
+    [ div [ class "ui eight wide column" ]
+        [ RandomGif.view (Signal.forwardTo address RandomGifPair.Update.Left) model.left ]
+    , div [ class "ui eight wide column" ]
+        [ RandomGif.view (Signal.forwardTo address RandomGifPair.Update.Right) model.right ]
     ]
